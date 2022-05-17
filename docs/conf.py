54da28598258d69fcf0fcdfb92bbed98d9a5fb4a
Andrew Calcutt <acalcutt@techidiots.net>
2022-05-17 13:41:07 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -43,8 +43,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'TileServer GL'
-copyright = u'2016, Klokan Technologies GmbH'
+project = u'Maplibre TileServer GL'
+copyright = u'2022 Andrew Calcutt 2016, Klokan Technologies GmbH'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
